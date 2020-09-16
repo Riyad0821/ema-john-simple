e06787fd0f2dbfab799e550e9a20f141c5ab1065
Riyad0821 <mdriyad0821@gmail.com>
2020-09-16 20:05:02 +0600

    complete

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -4,11 +4,11 @@ import fakeData from '../../fakeData';
 import Product from '../Product/Product';
 
 const ProductDetail = () => {
-    const {productKey} = useParams();
-    const product = fakeData.find(pd => pd.key === productKey);
+     const {productKey} = useParams();
+     const product = fakeData.find(pd => pd.key === productKey);
     return (
         <div>
-            <h1>{productKey} Your product details</h1>
+            <h1>Your product details</h1>
             <Product showAddToCart={false} product={product}></Product>
         </div>
     );
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,11 +1,27 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { getDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart, removeFromDatabaseCart, processOrder } from '../../utilities/databaseManager';
 import fakeData from '../../fakeData';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import Cart from '../Cart/Cart';
+import happyImage from '../../images/giphy.gif'
 
 const Review = () => {
     const [cart, setCart] = useState([]);
+    const [orderPlaced, setOrderPlaced] = useState(false);
+    const handlePlaceOrder = () => {
+        setCart([]);
+        setOrderPlaced(true);
+        processOrder();
+    }
+
+    const removeProduct = (productKey) => {
+        console.log('remove clicked', productKey);
+        const newCart = cart.filter(pd => pd.key !== productKey);
+        setCart(newCart);
+        removeFromDatabaseCart(productKey);
+    }
+
     useEffect(() => {
         const savedCart = getDatabaseCart();
         const productKeys = Object.keys(savedCart);
@@ -17,14 +33,27 @@ const Review = () => {
         });
         setCart(cartProducts);
     }, [])
+    let thankyou;
+    if(orderPlaced)
+     {
+         thankyou = <img src={happyImage}></img>
+        }
     return (
-        <div className="review-item">
-            <h1>Cart Items: {cart.length}</h1>
+        <div className="twin-container">
+            <div className="product-container">
             {
                 cart.map(pd => <ReviewItem
                 key={pd.key}
-                     product={pd}></ReviewItem>)
+                removeProduct = {removeProduct}
+                product={pd}></ReviewItem>)
             }
+            {thankyou}
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}>
+                    <button onClick={handlePlaceOrder} className="main-button">Place Order</button>
+                </Cart>
+            </div>
         </div>
     );
 };
